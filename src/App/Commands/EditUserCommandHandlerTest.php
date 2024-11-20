e19cd2896520956a4380adf14aee13e0b550342a
Dmitriy Smotrov <dsxack@gmail.com>
2024-11-20 16:02:41 +0100

    Add Logger tests to EditUserCommandHandlerTest and rewrite tests with using mocks

--- src/App/Commands/EditUserCommandHandlerTest.php
+++ src/App/Commands/EditUserCommandHandlerTest.php
@@ -2,87 +2,118 @@
 
 namespace DmitriySmotrov\Interview\App\Commands;
 
-use DmitriySmotrov\Interview\Adapters\FileLogger;
-use DmitriySmotrov\Interview\Adapters\InMemoryUserEmailUntrustedDomains;
-use DmitriySmotrov\Interview\Adapters\InMemoryUserNameStopWords;
-use DmitriySmotrov\Interview\Adapters\InMemoryUserRepository;
+use DmitriySmotrov\Interview\App\Services\Logger;
 use DmitriySmotrov\Interview\App\Services\UserInfoVerifier;
 use DmitriySmotrov\Interview\Domain\User\DateTime;
-use DmitriySmotrov\Interview\Domain\User\Domain;
 use DmitriySmotrov\Interview\Domain\User\Email;
+use DmitriySmotrov\Interview\Domain\User\ID;
 use DmitriySmotrov\Interview\Domain\User\Name;
 use DmitriySmotrov\Interview\Domain\User\Notes;
 use DmitriySmotrov\Interview\Domain\User\Repository;
+use DmitriySmotrov\Interview\Domain\User\Timestamps;
 use DmitriySmotrov\Interview\Domain\User\User;
 use PHPUnit\Framework\TestCase;
 
 class EditUserCommandHandlerTest extends TestCase {
     public function testHandle() {
-        $users = new InMemoryUserRepository();
-        $user = $this->newTestUser();
-        $users->create($user);
+        $user = User::fromStorage(
+            new ID(1),
+            new Name('johndoe1'),
+            new Email('johndoe1@domain.com'),
+            Timestamps::fromStorage(DateTime::now(), null),
+            new Notes('test notes'),
+        );
 
-        $handler = $this->newHandler($users, [], []);
+        $repoMock = $this->createMock(Repository::class);
+        $repoMock
+            ->expects($this->once())
+            ->method('update')
+            ->willReturnCallback(function ($id, $callback) use ($user) {
+                $this->assertEquals($user->id()->toInteger(), $id->toInteger());
+                $callback($user);
+                $this->assertEquals('johndoe2', $user->name()->toString());
+                $this->assertEquals('johndoe2@domain.com', $user->email()->toString());
+                $this->assertEquals('test new notes', $user->notes()->toString());
+            });
+
+        $userInfoVerifierMock = $this->createMock(UserInfoVerifier::class);
+        $userInfoVerifierMock
+            ->expects($this->once())
+            ->method('verify')
+            ->with(new Name('johndoe2'), new Email('johndoe2@domain.com'));
+
+        $loggerMock = $this->createMock(Logger::class);
+        $loggerMock
+            ->expects($this->once())
+            ->method('log')
+            ->with("Trying to edit User", [
+                "id" => 1,
+                "old_name" => 'johndoe1',
+                "old_email" => 'johndoe1@domain.com',
+                "old_notes" => 'test notes',
+                "new_name" => 'johndoe2',
+                "new_email" => 'johndoe2@domain.com',
+                "new_notes" => 'test new notes',
+            ]);
+
+        $handler = new EditUserCommandHandler($repoMock, $userInfoVerifierMock, $loggerMock);
         $handler->handle(new EditUserCommand(
             $user->id(),
             new Name('johndoe2'),
             new Email('johndoe2@domain.com'),
             new Notes('test new notes'),
         ));
-
-        $updatedUser = $users->find($user->id());
-        $this->assertEquals('johndoe2', $updatedUser->name()->toString());
-        $this->assertEquals('johndoe2@domain.com', $updatedUser->email()->toString());
-        $this->assertEquals('test new notes', $updatedUser->notes()->toString());
     }
 
     public function testHandleWithStopWordInUserName() {
-        $users = new InMemoryUserRepository();
-        $user = $this->newTestUser();
-        $users->create($user);
-        $handler = $this->newHandler(new InMemoryUserRepository(), ['stopWord'], []);
+        $repoMock = $this->createMock(Repository::class);
+
+        $userInfoVerifierMock = $this->createMock(UserInfoVerifier::class);
+        $userInfoVerifierMock
+            ->expects($this->once())
+            ->method('verify')
+            ->willThrowException(new UserNameContainsStopWordException());
+
+        $loggerMock = $this->createMock(Logger::class);
+        $loggerMock
+            ->expects($this->once())
+            ->method('log')
+            ->with("Attempt to edit user with stop word in name johndoe2");
+
+        $handler = new EditUserCommandHandler($repoMock, $userInfoVerifierMock, $loggerMock);
 
         $this->expectException(UserNameContainsStopWordException::class);
         $handler->handle(new EditUserCommand(
-            $user->id(),
-            new Name('johndoe1StopWord'),
-            new Email('johndoe1@domain.com'),
-            new Notes('test notes'),
+            new ID(1),
+            new Name('johndoe2'),
+            new Email('johndoe2@domain.com'),
+            new Notes('test new notes'),
         ));
     }
 
     public function testHandleWithUntrustedDomain() {
-        $users = new InMemoryUserRepository();
-        $user = $this->newTestUser();
-        $users->create($user);
+        $repoMock = $this->createMock(Repository::class);
 
-        $handler = $this->newHandler(new InMemoryUserRepository(), [], [new Domain('domain.com')]);
+        $userInfoVerifierMock = $this->createMock(UserInfoVerifier::class);
+        $userInfoVerifierMock
+            ->expects($this->once())
+            ->method('verify')
+            ->willThrowException(new UserEmailUntrustedDomainException());
+
+        $loggerMock = $this->createMock(Logger::class);
+        $loggerMock
+            ->expects($this->once())
+            ->method('log')
+            ->with("Attempt to edit user email with untrusted domain johndoe2@domain.com");
+
+        $handler = new EditUserCommandHandler($repoMock, $userInfoVerifierMock, $loggerMock);
 
         $this->expectException(UserEmailUntrustedDomainException::class);
         $handler->handle(new EditUserCommand(
-            $user->id(),
-            new Name('johndoe1StopWord'),
-            new Email('johndoe1@domain.com'),
-            new Notes('test notes'),
+            new ID(1),
+            new Name('johndoe2'),
+            new Email('johndoe2@domain.com'),
+            new Notes('test new notes'),
         ));
-    }
-
-    private function newHandler(Repository $users, array $stopWords, array $untrustedDomains): EditUserCommandHandler {
-        $logFile = fopen('php://memory', 'w+');
-        $logger = new FileLogger($logFile);
-        $stopWords = new InMemoryUserNameStopWords($stopWords);
-        $untrustedDomain = new InMemoryUserEmailUntrustedDomains($untrustedDomains);
-        $userInfoVerifier = new UserInfoVerifier($untrustedDomain, $stopWords);
-
-        return new EditUserCommandHandler($users, $userInfoVerifier, $logger);
-    }
-
-    public function newTestUser(): User {
-        return User::new(
-            new Name('johndoe1'),
-            new Email('johndoe1@domain.com'),
-            DateTime::now(),
-            new Notes('test notes'),
-        );
     }
 }
