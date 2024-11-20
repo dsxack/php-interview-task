1de89650c443d307552ae0d14242438a4894297d
Dmitriy Smotrov <dsxack@gmail.com>
2024-11-20 16:04:42 +0100

    Add Logger tests to UserQueryHandlerTest and rewrite tests with using mocks
    Also added negative test case

--- src/App/Queries/UserQueryHandlerTest.php
+++ src/App/Queries/UserQueryHandlerTest.php
@@ -2,29 +2,40 @@
 
 namespace DmitriySmotrov\Interview\App\Queries;
 
-use DmitriySmotrov\Interview\Adapters\FileLogger;
-use DmitriySmotrov\Interview\Adapters\InMemoryUserRepository;
+use DmitriySmotrov\Interview\App\Services\Logger;
 use DmitriySmotrov\Interview\Domain\User\DateTime;
 use DmitriySmotrov\Interview\Domain\User\Email;
+use DmitriySmotrov\Interview\Domain\User\ID;
 use DmitriySmotrov\Interview\Domain\User\Name;
 use DmitriySmotrov\Interview\Domain\User\Notes;
+use DmitriySmotrov\Interview\Domain\User\Timestamps;
 use DmitriySmotrov\Interview\Domain\User\User;
+use DmitriySmotrov\Interview\Domain\User\UserNotFoundException;
 use PHPUnit\Framework\TestCase;
 
 class UserQueryHandlerTest extends TestCase {
     public function testHandle() {
-        $users = new InMemoryUserRepository();
-        $user = User::new(
+        $user = User::fromStorage(
+            new ID(1),
             new Name('johndoe1'),
             new Email('johndoe1@domain.com'),
-            DateTime::now(),
+            Timestamps::fromStorage(DateTime::now(), null),
             new Notes('test notes'),
         );
-        $users->create($user);
 
-        $handler = $this->newHandler($users);
+        $repoMock = $this->createMock(UserQueryReadModel::class);
+        $repoMock
+            ->expects($this->once())
+            ->method('find')
+            ->with($user->id())
+            ->willReturn($user);
 
-        $this->assertNull($user->timestamps()->deletedAt());
+        $loggerMock = $this->createMock(Logger::class);
+        $loggerMock
+            ->expects($this->never())
+            ->method('log');
+
+        $handler = new UserQueryHandler($repoMock, $loggerMock);
         $userQueryResponse = $handler->handle(new UserQuery($user->id()));
 
         $this->assertEquals($user->id(), $userQueryResponse->id());
@@ -33,9 +44,25 @@ class UserQueryHandlerTest extends TestCase {
         $this->assertEquals($user->notes(), $userQueryResponse->notes());
     }
 
-    private function newHandler(UserQueryReadModel $users): UserQueryHandler {
-        $logFile = fopen('php://memory', 'w+');
-        $logger = new FileLogger($logFile);
-        return new UserQueryHandler($users, $logger);
+    public function testHandleUserNotFound() {
+        $id = new ID(1);
+
+        $repoMock = $this->createMock(UserQueryReadModel::class);
+        $repoMock
+            ->expects($this->once())
+            ->method('find')
+            ->with($id)
+            ->willReturn(null);
+
+        $loggerMock = $this->createMock(Logger::class);
+        $loggerMock
+            ->expects($this->once())
+            ->method('log')
+            ->with("User with ID 1 not found");
+
+        $handler = new UserQueryHandler($repoMock, $loggerMock);
+
+        $this->expectException(UserNotFoundException::class);
+        $handler->handle(new UserQuery($id));
     }
 }
